8a12bc42f1b75c74441f88dfac52141d4c0d9553
Opicksss <upek123slopeng@gmail.com>
2025-09-23 11:34:01 +0800

    dash

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,100 @@
+
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from themesdesign.in/tocly/layouts/layouts-icon-sidebar.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Sep 2025 01:32:11 GMT -->
+<head>
+
+        <meta charset="utf-8" />
+        <title>Icon Sidebar | Tocly - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- plugin css -->
+        <link href="assets/libs/jsvectormap/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Layout Js -->
+        <script src="assets/js/layout.js"></script>
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+
+
+    </head>
+
+    <body data-keep-enlarged="true" class="vertical-collpsed">
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+
+
+
+            @include('layouts.header')
+
+            <!-- ========== Left Sidebar Start ========== -->
+            @include('layouts.sidebar')
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                @yield('content')
+                <!-- End Page-content -->
+
+                @include('layouts.footer')
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Right Sidebar -->
+        @include('layouts.navbar')
+        <!-- /Right-bar -->
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <!-- Icon -->
+        <script src="unicons.iconscout.com/release/v2.0.1/script/monochrome/bundle.js"></script>
+
+        <!-- apexcharts -->
+        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+        <!-- Vector map-->
+        <script src="assets/libs/jsvectormap/jsvectormap.min.js"></script>
+        <script src="assets/libs/jsvectormap/maps/world-merc.js"></script>
+
+        <script src="assets/js/pages/dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.js"></script>
+
+        @include('layouts.scripts')
+
+        @yield('scripts')
+
+
+    </body>
+
+<!-- Mirrored from themesdesign.in/tocly/layouts/layouts-icon-sidebar.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Sep 2025 01:33:38 GMT -->
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,103 @@
+<div class="vertical-menu">
+
+                 <!-- LOGO -->
+                 <div class="navbar-brand-box">
+                    <a href="index.html" class="logo logo-dark">
+                        <span class="logo-sm">
+                            <img src="assets/images/logo-sm-dark.png" alt="logo-sm-dark" height="24">
+                        </span>
+                        <span class="logo-lg">
+                            <img src="assets/images/logo-dark.png" alt="logo-dark" height="22">
+                        </span>
+                    </a>
+
+                    <a href="index.html" class="logo logo-light">
+                        <span class="logo-sm">
+                            <img src="assets/images/logo-sm-light.png" alt="logo-sm-light" height="24">
+                        </span>
+                        <span class="logo-lg">
+                            <img src="assets/images/logo-light.png" alt="logo-light" height="22">
+                        </span>
+                    </a>
+                </div>
+
+                <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect vertical-menu-btn" id="vertical-menu-btn">
+                    <i class="ri-menu-2-line align-middle"></i>
+                </button>
+
+                <div data-simplebar class="vertical-scroll">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title">Menu</li>
+
+                            <li>
+                                <a href="index.html" class="waves-effect">
+                                    <i class="uim uim-airplay"></i><span class="badge rounded-pill bg-success float-end">3</span>
+                                    <span>Dashboard</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="uim uim-comment-message"></i>
+                                    <span>Apps</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="true">
+
+
+                                    <li><a href="calendar.html">Calendar</a></li>
+
+                                    <li><a href="apps-chat.html">Chat</a></li>
+
+                                    <li><a href="apps-file-manager.html">File Manager</a></li>
+
+
+
+                                </ul>
+                            </li>
+
+
+
+
+
+
+                        </ul>
+
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+
+                <div class="dropdown px-3 sidebar-user sidebar-user-info">
+                    <button type="button" class="btn w-100 px-0 border-0" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                    <img src="assets/images/users/avatar-2.jpg" class="img-fluid header-profile-user rounded-circle" alt="">
+                            </div>
+
+                            <div class="flex-grow-1 ms-2 text-start">
+                                <span class="ms-1 fw-medium user-name-text">Steven Deese</span>
+                            </div>
+
+                            <div class="flex-shrink-0 text-end">
+                                <i class="mdi mdi-dots-vertical font-size-16"></i>
+                            </div>
+                        </span>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <!-- item-->
+                        <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
+                        <a class="dropdown-item" href="apps-chat.html"><i class="mdi mdi-message-text-outline text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Messages</span></a>
+                        <a class="dropdown-item" href="pages-faq.html"><i class="mdi mdi-lifebuoy text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Help</span></a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-wallet text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Balance : <b>$5971.67</b></span></a>
+                        <a class="dropdown-item" href="#"><span class="badge bg-primary mt-1 float-end">New</span><i class="mdi mdi-cog-outline text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Settings</span></a>
+                        <a class="dropdown-item" href="auth-lock-screen.html"><i class="mdi mdi-lock text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Lock screen</span></a>
+                    </div>
+                </div>
+
+            </div>
