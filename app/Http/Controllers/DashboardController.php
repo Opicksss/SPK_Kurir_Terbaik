596c9332a09d2554db4dfcba73821a1acc30ca61
sovi <sovialfina@gmail.com>
2025-10-04 17:17:18 +0700

    dashboard proses

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kriteria;
+use App\Models\Kurir;
+use App\Models\Rekap;
+use App\Models\SubKriteria;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('welcome');
+        $kriteria = Kriteria::all();
+        $subKriteria = SubKriteria::all();
+        $kurir = Kurir::all();
+        $rekap = Rekap::all();
+
+        // Statistik untuk dashboard
+        $totalKriteria = Kriteria::count();
+        $totalKurir = Kurir::count();
+        $totalRekap = Rekap::count();
+        $totalSubKriteria = SubKriteria::count();
+
+
+
+        return view('dashboard.dashboard', compact(
+            'kriteria',
+            'subKriteria',
+            'kurir',
+            'rekap',
+            'totalKriteria',
+            'totalKurir',
+            'totalRekap',
+            'totalSubKriteria',
+
+        ));
     }
 }
